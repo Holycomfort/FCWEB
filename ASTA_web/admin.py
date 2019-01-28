4b041fcead9f4da9791db39a883a5452ec3fe406
Holycomfort <46879410+Holycomfort@users.noreply.github.com>
2019-01-28 20:08:45 +0800

    Update admin.py

--- ASTA_web/admin.py
+++ ASTA_web/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from .models import User
 from .models import Message
+from .models import Blog
+from .models import UploadFile
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Message)
+admin.site.register(Blog)
+admin.site.register(UploadFile)
