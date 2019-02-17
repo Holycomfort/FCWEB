69ae35bbb83ae2df69a99abd9028d2f6a05c6269
Holycomfort <46879410+Holycomfort@users.noreply.github.com>
2019-02-17 23:20:30 +0800

    Update admin.py

--- ASTA_web/admin.py
+++ ASTA_web/admin.py
@@ -3,9 +3,11 @@ from .models import User
 from .models import Message
 from .models import Blog
 from .models import UploadFile
+from .models import Rank
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Message)
 admin.site.register(Blog)
 admin.site.register(UploadFile)
+admin.site.register(Rank)
